8a7f56dd206f606338f4086f80e27c6cb10129bc
tfadlyr <tariq.fadly@gmail.com>
2024-04-22 22:31:39 +0700

    yes

--- resources/js/components/Dashboard.jsx
+++ resources/js/components/Dashboard.jsx
@@ -43,61 +43,65 @@ const Dashboard = () => {
                 <Grid container spacing={2} sx={{padding: 5, paddingX: 10}}>
                     <Grid item alignItems='center' justifyContent='center' xs={6}>
                         <Stack flex={1}>
-                            <Box sx={{backgroundColor: '#232738', height: 298, maxWidth: 850, marginLeft: 8}}>
-                                <Box sx={{backgroundColor: '#272E47', height: 40}}>
+                            <Box sx={{backgroundColor: '#232738', height: 298, maxWidth: 850, marginLeft: 8, borderRadius: 3}}>
+                                <Box sx={{backgroundColor: '#272E47', height: 40, paddingTop: 1, borderTopLeftRadius: 12, borderTopRightRadius: 12}}>
                                     <Grid container>
                                         <Grid item xs={7}>
-                                            <Typography sx={{color: "#FFFFFF"}}>Most Played Amongst Gamers</Typography>
+                                            <Typography sx={{color: "#FFFFFF", textAlign: 'center'}}>Most Played Amongst Gamers</Typography>
                                         </Grid>
                                         <Grid item xs={3}>
-                                            <Typography sx={{color: "#FFFFFF"}}>Peak players</Typography>
+                                            <Typography sx={{color: "#FFFFFF", textAlign: 'center'}}>Peak players</Typography>
                                         </Grid>
                                         <Grid item xs={2}>
-                                            <Typography sx={{color: "#FFFFFF"}}>In-game</Typography>
+                                            <Typography sx={{color: "#FFFFFF", textAlign: 'center'}}>In-game</Typography>
+                                        </Grid>
+                                    </Grid>
+                                </Box>
+                                <Game_Component pic="https://via.placeholder.com/93x44" name="CS" peak="100" current="20" />
+                            </Box>
+                        </Stack>
+                    </Grid>
+                    <Grid item alignItems='center' justifyContent='center' xs={6}>
+                        <Box sx={{backgroundColor: '#232738', height: 298, maxWidth: 850, borderRadius: 3}}>
+                            <Box sx={{backgroundColor: '#272E47', height: 40, paddingTop: 1, borderTopLeftRadius: 12, borderTopRightRadius: 12}}>
+                                <Grid container>
+                                    <Grid item xs={7}>
+                                        <Typography sx={{color: "#FFFFFF", textAlign: 'center'}}>New Released Games</Typography>
+                                    </Grid>
+                                    <Grid item xs={3}>
+                                        <Typography sx={{color: "#FFFFFF", textAlign: 'center'}}>Peak players</Typography>
+                                    </Grid>
+                                    <Grid item xs={2}>
+                                        <Typography sx={{color: "#FFFFFF", textAlign: 'center'}}>In-game</Typography>
+                                    </Grid>
+                                </Grid>
+                            </Box>
+                            <Game_Component pic="https://via.placeholder.com/93x44" name="CS" peak="100" current="20" />
+                            <Game_Component pic="https://via.placeholder.com/93x44" name="CS" peak="100" current="20" />
+                        </Box>
+                    </Grid>
+                    <Grid item alignItems='center' justifyContent='center' xs={12}>
+                        <Stack alignItems='center' justifyContent='center' flex={1}>
+                            <Box sx={{backgroundColor: '#232738', height: 298, width: 950, borderRadius: 3}}>
+                                <Box sx={{backgroundColor: '#272E47', height: 40, paddingTop: 1, borderTopLeftRadius: 12, borderTopRightRadius: 12}}>
+                                    
+                                    <Grid container>
+                                        <Grid item xs={6}>
+                                            <Typography sx={{color: "#FFFFFF", textAlign: 'center'}}>Games on Discount</Typography>
+                                        </Grid>
+                                        <Grid item xs={2}>
+                                            <Typography sx={{color: "#FFFFFF", textAlign: 'center'}}>Original price</Typography>
+                                        </Grid>
+                                        <Grid item xs={2}>
+                                            <Typography sx={{color: "#FFFFFF", textAlign: 'center'}}>On discount</Typography>
+                                        </Grid>
+                                        <Grid item xs={2}>
+                                            <Typography sx={{color: "#FFFFFF", textAlign: 'center'}}>Ends on</Typography>
                                         </Grid>
                                     </Grid>
                                 </Box>
                             </Box>
                         </Stack>
-                    </Grid>
-                    <Grid item alignItems='center' justifyContent='center' xs={6}>
-                        <Box sx={{backgroundColor: '#232738', height: 298, maxWidth: 850}}>
-                            <Box sx={{backgroundColor: '#272E47', height: 40}}>
-                                <Grid container>
-                                    <Grid item xs={7}>
-                                        <Typography sx={{color: "#FFFFFF"}}>New Released Games</Typography>
-                                    </Grid>
-                                    <Grid item xs={3}>
-                                        <Typography sx={{color: "#FFFFFF"}}>Peak players</Typography>
-                                    </Grid>
-                                    <Grid item xs={2}>
-                                        <Typography sx={{color: "#FFFFFF"}}>In-game</Typography>
-                                    </Grid>
-                                </Grid>
-                            </Box>
-                            <Game_Component pic="https://via.placeholder.com/93x44" name="CS" peak="100" current="20" />
-                        </Box>
-                    </Grid>
-                    <Grid item alignItems='center' justifyContent='center' xs={12}>
-                        <Box sx={{backgroundColor: '#232738', height: 298, maxWidth: 950}}>
-                            <Box sx={{backgroundColor: '#272E47', height: 40}}>
-                                
-                                <Grid container>
-                                    <Grid item xs={6}>
-                                        <Typography sx={{color: "#FFFFFF"}}>Games on Discount</Typography>
-                                    </Grid>
-                                    <Grid item xs={2}>
-                                        <Typography sx={{color: "#FFFFFF"}}>Original price</Typography>
-                                    </Grid>
-                                    <Grid item xs={2}>
-                                        <Typography sx={{color: "#FFFFFF"}}>On discount</Typography>
-                                    </Grid>
-                                    <Grid item xs={2}>
-                                        <Typography sx={{color: "#FFFFFF"}}>Ends on</Typography>
-                                    </Grid>
-                                </Grid>
-                            </Box>
-                        </Box>
                     </Grid>
                 </Grid>
                 
--- resources/js/components/Game_Component.jsx
+++ resources/js/components/Game_Component.jsx
@@ -4,20 +4,20 @@ import { Box, Grid, Stack, Typography } from "@mui/material";
 const Game_Componenet = ({ pic, name, peak, current}) => {
     return (
         <>
-            <Box sx={{backgroundColor: '#232738', height: 50, maxWidth: 850, '&:hover': { cursor: 'pointer'}}}>
+            <Box sx={{backgroundColor: '#232738', height: 50, maxWidth: 850, marginY: 1, '&:hover': { cursor: 'pointer'}}}>
                 <Stack direction='row' >
-                    <Grid container>
+                    <Grid container sx={{marginX: 2, marginY: 1}}>
                         <Grid item xs={7}>
-                            <Stack direction='row'>
+                            <Stack direction='row' alignItems='center' spacing={2}>
                                 <img src={pic}/>
                                 <Typography sx={{color: "#FFFFFF"}}>{name}</Typography>
                             </Stack>
                         </Grid>
                         <Grid item xs={3}>
-                            <Typography sx={{color: "#FFFFFF"}}>{peak}</Typography>
+                            <Typography sx={{color: "#FFFFFF" , textAlign: 'center'}}>{peak}</Typography>
                         </Grid>
                         <Grid item xs={2}>
-                            <Typography sx={{color: "#FFFFFF"}}>{current}</Typography>
+                            <Typography sx={{color: "#FFFFFF" , textAlign: 'center'}}>{current}</Typography>
                         </Grid>
                     </Grid>
                 </Stack>
