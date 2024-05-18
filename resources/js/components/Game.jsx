d935ef75c6a16c317879332ed463b32ef9c083ec
Gazell Arrafi <122022916+GazellArrafi@users.noreply.github.com>
2024-05-18 13:41:28 +0700

    add star darkMode and filled

--- resources/js/components/Game.jsx
+++ resources/js/components/Game.jsx
@@ -1,6 +1,5 @@
-import React from "react";
+import React, { useState, useEffect } from 'react';
 import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Box, Typography, TextField, Grid, Paper, Stack} from '@mui/material'
-import { useState } from "react";
 import '../../css/app.css';
 import { auto } from "@popperjs/core";
 import UpdateInGame from "./UpdateInGame";
@@ -10,15 +9,47 @@ import InsertGameStatistic from "./InsertGameStatistic";
 
 import LightMode from "../../../public/Light_Mode.png";
 import DarkMode from "../../../public/Dark_Mode.png";
+import FilledStarD from "../../../public/yellow_star.png";
+import FilledStarL from "../../../public/filled_star.png";
+import BlankStar from "../../../public/blank_star.png";
+
 
 const Game = () => {
     const [darkMode, setDarkMode] = useState(true);
     const [imageSource, setImageSource] = useState(DarkMode);
+    const [darkModeS, setDarkModeS] = useState(true);
+    const [imageSourceF, setImageSourceF] = useState(FilledStarD);
+    const [changeStar, setChangeStar] = useState(BlankStar);
+    const [keyImg, setKeyImg] = useState(2);  
+
+    useEffect(() => {
+        setImageSource(darkMode ? DarkMode : LightMode);
+        setImageSourceF(darkMode ? FilledStarD : FilledStarL);
+        if (changeStar !== BlankStar) {
+            setChangeStar(darkMode ? FilledStarD : FilledStarL);
+        }
+    }, [darkMode]);
 
     const toggleDarkMode = () => {
-        setDarkMode(!darkMode);
+        setDarkMode(prevMode => !prevMode);
+        setDarkModeS(prevMode => !prevMode);
         setImageSource(darkMode ? LightMode : DarkMode);
+        setImageSourceF(darkModeS ? FilledStarL : FilledStarD);
+        if (changeStar !== BlankStar) {
+            setChangeStar(darkMode ? FilledStarL : FilledStarD);
+        }
     };
+    
+    const toggleChangeStar = () => {
+    if (changeStar === BlankStar) {
+        setChangeStar(imageSourceF);
+        setKeyImg(2);
+    } else {
+        setChangeStar(BlankStar);
+        setKeyImg(1);
+    }
+    };
+
     return (
         <>
             <Box sx={{backgroundColor: darkMode ? '#13151e' : '#f09d30', width: auto}}>
@@ -34,26 +65,31 @@ const Game = () => {
                 </Paper>
             </Box>
             <Box sx={{backgroundColor: darkMode ? '#13151e' : '#f09d30', width: auto, maxHeight: auto, padding: 10, g: 10 }}>
-                <Stack direction="column" justifyContent="flex-start" spacing={1}>
-                    <Typography sx={{color: "var(--main-text-color)", fontSize: 48, fontWeight: 'bold', '&:hover': { cursor: 'pointer'}}}>Counter-Strike 2</Typography>
-                </Stack>
+                <Grid sx={{display: 'flex', gap: 10}}>
+                    <Stack direction="column" justifyContent="flex-start" spacing={1}>
+                        <Typography sx={{color: darkMode ? "var(--main-text-color)" : '#985027', fontSize: 48, fontWeight: 'bold', '&:hover': { cursor: 'pointer'}}}>Counter-Strike 2</Typography>
+                    </Stack>
+                    <Stack display='flex' direction="row" alignItems='center' justifyContent="flex-start" spacing={1}>
+                        <img src={changeStar} key={keyImg} style={{width: 50, height: 50}} onClick={toggleChangeStar}/>
+                        <Typography sx={{color: darkMode ? "var(--main-text-color)" : '#985027', fontSize: 48, fontWeight: 'bold', '&:hover': { cursor: 'pointer'}}}>Favorites</Typography>
+                    </Stack>
+                </Grid>
                 <Grid container sx={{display: 'flex', justifyContent:'space-between', paddingTop: 10}} spacing={10}>
                     <Grid item xs={2.5} sx={{display: 'flex', flexDirection: 'column', justifyContent: 'space-between'}}>
                         <img src="https://via.placeholder.com/300x300" alt="Dummy Image"/>
-                        <Grid sx={{color: 'var(--main-text-color)', textAlign: 'center', }}>
+                        <Grid sx={{color: darkMode ? "var(--main-text-color)" : '#985027', textAlign: 'center', }}>
                             <Typography variant="h6">Users rating:</Typography>
                             <Typography variant="h6">8,7 of 10</Typography>
                         </Grid>
                     </Grid>
                     <Grid item xs={4.5} sx={{display: 'flex', flexDirection: 'column', justifyContent: 'space-between'}}>
-                        <Grid sx={{color: 'var(--main-text-color)', marginBottom: 2}}>
+                        <Grid sx={{color: darkMode ? "var(--main-text-color)" : '#985027', marginBottom: 2}}>
                             <Typography variant="h3">Game Description </Typography>
                             <Typography variant="h6" sx={{ overflow: 'auto', maxHeight: 250 }}>
                                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo dignissimos molestias laudantium saepe distinctio aliquam provident est delectus ullam repellendus. ipsum dolor sit, amet consectetur adipisicing elit. Quisquam quae inventore quas cupiditate dolor, velit aperiam nulla quo qui adipisci consequatur illo beatae tempore perferendis eum natus ratione veritatis deleniti ducimus reiciendis nihil nostrum modi? Possimus harum delectus sit totam minus sequi a. Veniam ea aperiam, fuga commodi quasi eum consequatur molestias natus nobis libero voluptas rem quibusdam sed reiciendis illum incidunt ipsa perspiciatis nisi voluptatibus! Facilis provident tempore totam?
                             </Typography>
-
                         </Grid>
-                        <Grid sx={{color: 'var(--main-text-color)', display: 'flex', justifyContent: 'center', gap: 10, }}>
+                        <Grid sx={{color: darkMode ? "var(--main-text-color)" : '#985027', display: 'flex', justifyContent: 'center', gap: 10, }}>
                             <Grid sx={{textAlign: 'center'}}>
                                 <Typography variant="h6">In-game</Typography>
                                 <Typography variant="h6">809,142</Typography>
@@ -68,36 +104,36 @@ const Game = () => {
                         </Grid>
                     </Grid>
                     <Grid item xs={5}>
-                        <Stack sx={{color: 'var(--main-text-color)',border: '0.5px solid', borderColor: '#2f396f', justifyContent: 'space-between', padding: 0.5}}>
-                            <Grid item className="var1GamePage">
+                        <Stack sx={{color: darkMode ? "var(--main-text-color)" : '#985027', justifyContent: 'space-between', padding: 0.5}}>
+                            <Grid item className="var1GamePage" sx={{backgroundColor: darkMode ? 'var(--var1-gamepage)' : '#D67138'}}>
                                 <Typography variant="h6">Developer</Typography>
                                 <Typography variant="h6">Valve Industries</Typography>
                             </Grid>
-                            <Grid item className="var2GamePage">
+                            <Grid item className="var2GamePage" sx={{backgroundColor: darkMode ? 'var(--var2-gamepage)' : '#B9602E'}}>
                                 <Typography variant="h6">Publisher</Typography>
                                 <Typography variant="h6">Valve Industries</Typography>
                             </Grid>
-                            <Grid item className="var1GamePage">
+                            <Grid item className="var1GamePage" sx={{backgroundColor: darkMode ? 'var(--var1-gamepage)' : '#D67138'}}>
                                 <Typography variant="h6">Supported Systems</Typography>
                                 <Typography variant="h6">Wingodswes</Typography>
                             </Grid>
-                            <Grid item className="var2GamePage">
+                            <Grid item className="var2GamePage" sx={{backgroundColor: darkMode ? 'var(--var2-gamepage)' : '#B9602E'}}>
                                 <Typography variant="h6">Technologies / Game engine</Typography>
                                 <Typography variant="h6">Source 3.0</Typography>
                             </Grid>
-                            <Grid item className="var1GamePage">
+                            <Grid item className="var1GamePage" sx={{backgroundColor: darkMode ? 'var(--var1-gamepage)' : '#D67138'}}>
                                 <Typography variant="h6">Last update</Typography>
                                 <Typography variant="h6">teuing iraha</Typography>
                             </Grid>
-                            <Grid item className="var2GamePage">
+                            <Grid item className="var2GamePage" sx={{backgroundColor: darkMode ? 'var(--var2-gamepage)' : '#B9602E'}}>
                                 <Typography variant="h6">Release date</Typography>
                                 <Typography variant="h6">teu apal</Typography>
                             </Grid>
-                            <Grid item className="var1GamePage">
+                            <Grid item className="var1GamePage" sx={{backgroundColor: darkMode ? 'var(--var1-gamepage)' : '#D67138'}}>
                                 <Typography variant="h6">Game Genre</Typography>
                                 <Typography variant="h6">teuing</Typography>
                             </Grid>
-                            <Grid item className="var2GamePage">
+                            <Grid item className="var2GamePage" sx={{backgroundColor: darkMode ? 'var(--var2-gamepage)' : '#B9602E'}}>
                                 <Typography variant="h6">Age rating</Typography>
                                 <Typography variant="h6">22+</Typography>
                             </Grid>
@@ -106,21 +142,22 @@ const Game = () => {
                 </Grid>
 
                 <Grid sx={{paddingTop: 10}}>
-                    <Box sx={{border: '1px solid white', borderRadius: 10, paddingX: 5, display: 'flex', alignItems: 'center', justifyContent: 'space-around'}}>
-                        <Typography sx={{fontSize: 32, color: 'var(--main-text-color)'}}>Game Statistic</Typography>
-                        <Grid sx={{display: 'flex',  alignItems: 'center', gap: 5}}>
-                            <Stack direction='row' sx={{gap: 1}}>
-                                <Typography sx={{color: 'var(--main-text-color)'}}>Year</Typography>
-                                <Typography sx={{color: 'var(--main-text-color)'}}>|</Typography>
-                                <Typography sx={{color: 'var(--main-text-color)'}}>Month</Typography>
-                                <Typography sx={{color: 'var(--main-text-color)'}}>|</Typography>
-                                <Typography sx={{color: 'var(--main-text-color)'}}>Peak</Typography>
-                                <Typography sx={{color: 'var(--main-text-color)'}}>|</Typography>
-                                <Typography sx={{color: 'var(--main-text-color)'}}>Gain</Typography>
-                            </Stack>
-                            <InsertGameStatistic/>
-                        </Grid>
+                <Box sx={{border: `1px solid ${darkMode ? '#FFFFFF' : '#985027'}`,borderRadius: 10, paddingX: 5, display: 'flex', alignItems: 'center', justifyContent: 'space-around'}}>
+                    <Typography sx={{fontSize: 32, color: darkMode ? 'var(--main-text-color)' : '#985027'}}>Game Statistic</Typography>
+                    <Grid sx={{display: 'flex', alignItems: 'center', gap: 5}} >
+                        <Stack direction='row' sx={{ gap: 1 }}>
+                        <Typography sx={{ color: darkMode ? 'var(--main-text-color)' : '#985027' }}>Year</Typography>
+                        <Typography sx={{ color: darkMode ? 'var(--main-text-color)' : '#985027' }}>|</Typography>
+                        <Typography sx={{ color: darkMode ? 'var(--main-text-color)' : '#985027' }}>Month</Typography>
+                        <Typography sx={{ color: darkMode ? 'var(--main-text-color)' : '#985027' }}>|</Typography>
+                        <Typography sx={{ color: darkMode ? 'var(--main-text-color)' : '#985027' }}>Peak</Typography>
+                        <Typography sx={{ color: darkMode ? 'var(--main-text-color)' : '#985027' }}>|</Typography>
+                        <Typography sx={{ color: darkMode ? 'var(--main-text-color)' : '#985027' }}>Gain</Typography>
+                        </Stack>
+                        <InsertGameStatistic />
+                    </Grid>
                     </Box>
+
                     <Box sx={{display: 'flex', justifyContent: 'space-around', marginTop: 4}}>
                         <Grid container spacing={2}>
                             <Grid item xs={12}>
@@ -128,40 +165,40 @@ const Game = () => {
                                     <Table sx={{backgroundColor: darkMode ? '#13151e' : '#f09d30'}}>
                                         <TableHead>
                                             <TableRow>
-                                                <TableCell sx={{color: 'var(--main-text-color)'}}>Year</TableCell>
-                                                <TableCell sx={{color: 'var(--main-text-color)'}}>Month</TableCell>
-                                                <TableCell sx={{color: 'var(--main-text-color)'}}>Month</TableCell>
-                                                <TableCell sx={{color: 'var(--main-text-color)'}}>Month</TableCell>
+                                                <TableCell sx={{color: darkMode ? "var(--main-text-color)" : '#985027'}}>Year</TableCell>
+                                                <TableCell sx={{color: darkMode ? "var(--main-text-color)" : '#985027'}}>Month</TableCell>
+                                                <TableCell sx={{color: darkMode ? "var(--main-text-color)" : '#985027'}}>Month</TableCell>
+                                                <TableCell sx={{color: darkMode ? "var(--main-text-color)" : '#985027'}}>Month</TableCell>
                                             </TableRow>
                                         </TableHead>
                                         <TableBody>
                                             <TableRow>
-                                                <TableCell sx={{color: 'var(--main-text-color)'}}>2024</TableCell>
+                                                <TableCell sx={{color: darkMode ? "var(--main-text-color)" : '#985027'}}>2024</TableCell>
                                                 <TableCell>
                                                     <Stack spacing={1}>
-                                                        <Typography sx={{color: 'var(--main-text-color)'}}>April</Typography>
-                                                        <Typography sx={{color: 'var(--main-text-color)'}}>March</Typography>
-                                                        <Typography sx={{color: 'var(--main-text-color)'}}>February</Typography>
-                                                        <Typography sx={{color: 'var(--main-text-color)'}}>January</Typography>
+                                                        <Typography sx={{color: darkMode ? "var(--main-text-color)" : '#985027'}}>April</Typography>
+                                                        <Typography sx={{color: darkMode ? "var(--main-text-color)" : '#985027'}}>March</Typography>
+                                                        <Typography sx={{color: darkMode ? "var(--main-text-color)" : '#985027'}}>February</Typography>
+                                                        <Typography sx={{color: darkMode ? "var(--main-text-color)" : '#985027'}}>January</Typography>
                                                     </Stack>
                                                 </TableCell>
                                             </TableRow>
                                             <TableRow>
-                                                <TableCell sx={{color: 'var(--main-text-color)'}}>2023</TableCell>
+                                                <TableCell sx={{color: darkMode ? "var(--main-text-color)" : '#985027'}}>2023</TableCell>
                                                 <TableCell>
                                                 <Stack spacing={1}>
-                                                    <Typography sx={{color: 'var(--main-text-color)'}}>December</Typography>
-                                                    <Typography sx={{color: 'var(--main-text-color)'}}>November</Typography>
-                                                    <Typography sx={{color: 'var(--main-text-color)'}}>October</Typography>
-                                                    <Typography sx={{color: 'var(--main-text-color)'}}>September</Typography>
-                                                    <Typography sx={{color: 'var(--main-text-color)'}}>August</Typography>
-                                                    <Typography sx={{color: 'var(--main-text-color)'}}>July</Typography>
-                                                    <Typography sx={{color: 'var(--main-text-color)'}}>June</Typography>
-                                                    <Typography sx={{color: 'var(--main-text-color)'}}>May</Typography>
-                                                    <Typography sx={{color: 'var(--main-text-color)'}}>April</Typography>
-                                                    <Typography sx={{color: 'var(--main-text-color)'}}>March</Typography>
-                                                    <Typography sx={{color: 'var(--main-text-color)'}}>February</Typography>
-                                                    <Typography sx={{color: 'var(--main-text-color)'}}>January</Typography>
+                                                    <Typography sx={{color: darkMode ? "var(--main-text-color)" : '#985027'}}>December</Typography>
+                                                    <Typography sx={{color: darkMode ? "var(--main-text-color)" : '#985027'}}>November</Typography>
+                                                    <Typography sx={{color: darkMode ? "var(--main-text-color)" : '#985027'}}>October</Typography>
+                                                    <Typography sx={{color: darkMode ? "var(--main-text-color)" : '#985027'}}>September</Typography>
+                                                    <Typography sx={{color: darkMode ? "var(--main-text-color)" : '#985027'}}>August</Typography>
+                                                    <Typography sx={{color: darkMode ? "var(--main-text-color)" : '#985027'}}>July</Typography>
+                                                    <Typography sx={{color: darkMode ? "var(--main-text-color)" : '#985027'}}>June</Typography>
+                                                    <Typography sx={{color: darkMode ? "var(--main-text-color)" : '#985027'}}>May</Typography>
+                                                    <Typography sx={{color: darkMode ? "var(--main-text-color)" : '#985027'}}>April</Typography>
+                                                    <Typography sx={{color: darkMode ? "var(--main-text-color)" : '#985027'}}>March</Typography>
+                                                    <Typography sx={{color: darkMode ? "var(--main-text-color)" : '#985027'}}>February</Typography>
+                                                    <Typography sx={{color: darkMode ? "var(--main-text-color)" : '#985027'}}>January</Typography>
                                                 </Stack>
                                                 </TableCell>
                                             </TableRow>
